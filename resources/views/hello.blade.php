0bedd1cd18291111f20713b4897467f41b214be5
Mohamed Kamel <mokammel0000@gmail.com>
2023-07-04 20:23:14 +0300

    some updates

--- resources/views/hello.blade.php
+++ resources/views/hello.blade.php
@@ -32,11 +32,7 @@
             font-size: 1.7rem !important;
         }
     </style>
-
-    {{-- bootstrap --}}
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
-
+    <link rel="stylesheet" type="text/css" href="{{asset('assets/css/bootstrap.css')}}">
     {{-- font-awesome --}}
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css" integrity="sha512-SzlrxWUlpfuzQ+pcUCosxcglQRNAq/DZjVsC0lE40xsADsfeQoEypE+enwcOiGjk/bSuGGKHEyjSoQ1zVisanQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
@@ -133,5 +129,6 @@
             </div>
         </div>
     </div>
+    <script src="{{asset('assets/js/bootstrap.js')}}"></script>
 </body>
 </html>
